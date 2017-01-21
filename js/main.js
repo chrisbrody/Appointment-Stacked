c41bf4758dac67f19f2131107184964b093155ba
Chris Brody <chrisbrody@codifyacademy.com>
2017-01-21 11:32:36 -0800

    added firebase scripts

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+ // Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBGcWzbe7zlN7W5p8UwuyP41yif1_1f5Vk",
+  authDomain: "apptstacked.firebaseapp.com",
+  databaseURL: "https://apptstacked.firebaseio.com",
+  storageBucket: "apptstacked.appspot.com",
+  messagingSenderId: "744735320469"
+}
+firebase.initializeApp(config);
 
+console.log(firebase)
