47ff2a89c56273e5a48c5a4b3cb624457a5f21bb
Chris Brody <chrisbrody@codifyacademy.com>
2017-01-21 12:10:33 -0800

    added JavaScript and HTML code for google auth and firebase connection

--- js/main.js
+++ js/main.js
@@ -1,3 +1,108 @@
+// Your Client ID can be retrieved from your project in the Google
+// Developer Console, https://console.developers.google.com
+var CLIENT_ID = '253329261133-9dv2ckogg42o19b4t2qfp0ophheamiji.apps.googleusercontent.com';
+
+var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+
+/**
+ * Check if current user has authorized this application.
+ */
+function checkAuth() {
+  gapi.auth.authorize(
+    {
+      'client_id': CLIENT_ID,
+      'scope': SCOPES.join(' '),
+      'immediate': true
+    }, handleAuthResult);
+  // console.log('checkAuth Ran')
+}
+
+/**
+ * Handle response from authorization server.
+ *
+ * @param {Object} authResult Authorization result.
+ */
+function handleAuthResult(authResult) {
+  var authorizeDiv = document.getElementById('authorize-div');
+  if (authResult && !authResult.error) {
+    // Hide auth UI, then load client library.
+    authorizeDiv.style.display = 'none';
+    loadCalendarApi();
+  } else {
+    // Show auth UI, allowing the user to initiate authorization by
+    // clicking authorize button.
+    authorizeDiv.style.display = 'inline';
+  }
+}
+
+/**
+ * Initiate auth flow in response to user clicking authorize button.
+ *
+ * @param {Event} event Button click event.
+ */
+function handleAuthClick(event) {
+  gapi.auth.authorize(
+    {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+    handleAuthResult);
+  return false;
+}
+
+/**
+ * Load Google Calendar client library. List upcoming events
+ * once client library is loaded.
+ */
+function loadCalendarApi() {
+  gapi.client.load('calendar', 'v3', listUpcomingEvents);
+}
+
+/**
+ * Print the summary and start datetime/date of the next ten events in
+ * the authorized user's calendar. If no events are found an
+ * appropriate message is printed.
+ */
+function listUpcomingEvents() {
+  var request = gapi.client.calendar.events.list({
+    'calendarId': 'primary',
+    'timeMin': (new Date()).toISOString(),
+    'showDeleted': false,
+    'singleEvents': true,
+    'maxResults': 10,
+    'orderBy': 'startTime'
+  });
+
+  request.execute(function(resp) {
+    var events = resp.items;
+    appendPre('Upcoming events:');
+
+    if (events.length > 0) {
+      for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        var when = event.start.dateTime;
+        if (!when) {
+          when = event.start.date;
+        }
+        appendPre(event.summary + ' (' + when + ')')
+      }
+    } else {
+      appendPre('No upcoming events found.');
+    }
+
+  });
+}
+
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+  var pre = document.getElementById('output');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
+}
+
+
  // Initialize Firebase
 var config = {
   apiKey: "AIzaSyBGcWzbe7zlN7W5p8UwuyP41yif1_1f5Vk",
@@ -8,4 +113,143 @@ var config = {
 }
 firebase.initializeApp(config);
 
-console.log(firebase)
+/**
+ * Function called when clicking the Login/Logout button.
+ */
+// [START buttoncallback]
+function toggleSignIn() {
+  if (!firebase.auth().currentUser) {
+    // [START createprovider]
+    var provider = new firebase.auth.GoogleAuthProvider();
+    // [END createprovider]
+    // [START addscopes]
+    provider.addScope('https://www.googleapis.com/auth/plus.login');
+    // [END addscopes]
+    // [START signin]
+    firebase.auth().signInWithRedirect(provider);
+    // [END signin]
+  } else {
+    // [START signout]
+    firebase.auth().signOut();
+    // [END signout]
+  }
+  // [START_EXCLUDE]
+  document.getElementById('quickstart-sign-in').disabled = true;
+  loadCalendarApi()
+  // [END_EXCLUDE]
+}
+// [END buttoncallback]
+
+/**
+ * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+ *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+ *    out, and that is where we update the UI.
+ *  - firebase.auth().getRedirectResult(): This promise completes when the user gets back from
+ *    the auth redirect flow. It is where you can get the OAuth access token from the IDP.
+ */
+function initApp() {
+  // Result from Redirect auth flow.
+  // [START getidptoken]
+  firebase.auth().getRedirectResult().then(function(result) {
+    if (result.credential) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // [START_EXCLUDE]
+      document.getElementById('quickstart-oauthtoken').textContent = token;
+    } else {
+      document.getElementById('quickstart-oauthtoken').textContent = 'null';
+      // [END_EXCLUDE]
+    }
+    // The signed-in user info.
+    var user = result.user;
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // [START_EXCLUDE]
+    if (errorCode === 'auth/account-exists-with-different-credential') {
+      alert('You have already signed up with a different auth provider for that email.');
+      // If you are using multiple auth providers on your app you should handle linking
+      // the user's accounts here.
+    } else {
+      console.error(error);
+    }
+    // [END_EXCLUDE]
+  });
+  // [END getidptoken]
+
+// Listening for auth state changes.
+// [START authstatelistener]
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // [START_EXCLUDE]
+      document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+      document.getElementById('quickstart-sign-in').textContent = 'Sign out';
+      document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+      // [END_EXCLUDE]
+    } else {
+      // User is signed out.
+      // [START_EXCLUDE]
+      document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
+      document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
+      document.getElementById('quickstart-account-details').textContent = 'null';
+      document.getElementById('quickstart-oauthtoken').textContent = 'null';
+      // [END_EXCLUDE]
+    }
+    // [START_EXCLUDE]
+    document.getElementById('quickstart-sign-in').disabled = false;
+    // [END_EXCLUDE]
+  });
+  // [END authstatelistener]
+
+  document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+}
+
+window.onload = function() {
+    initApp();
+};
+
+
+ // anonymous function
+(function() {
+    // confirm firebase exists
+    console.log(firebase)
+
+    // add data to db
+    function writeUserData() {
+      var x = document.getElementById('emailMessage').value
+      console.log(x)
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            console.log(user)
+            firebase.database().ref('users/' + user.uid).set({
+                username: user.displayName,
+                email: user.email,
+                message: x,
+                active : true
+            });
+            alert('message updated')
+        }
+      })
+      
+    }
+
+    document.getElementById('emailMessageBtn').addEventListener('click', writeUserData)
+
+}())
+
+
+
+
